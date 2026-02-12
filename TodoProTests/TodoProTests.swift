a7855758a36ca0c8d448da8708860a04d451f624
Arkin Azizi <arkinmit@gmail.com>
2026-02-12 11:59:03 +0300

    Initial Commit

---
+++ TodoProTests/TodoProTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoProTests.swift
+//  TodoProTests
+//
+//  Created by Arkin Azizi on 12.02.2026.
+//
+
+import Testing
+@testable import TodoPro
+
+struct TodoProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
